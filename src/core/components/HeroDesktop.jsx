5a619fed296dd847b0c9a53986af0f9f8ebb121f
Yazid Al Ghozali <yazid.al2418@gmail.com>
2025-12-16 15:56:17 +0700

    feat: changes in hero desktop

--- src/core/components/HeroDesktop.jsx
+++ src/core/components/HeroDesktop.jsx
@@ -8,7 +8,7 @@ export default function HeroDesktop() {
             <div className="grid grid-cols-2 p-8 place-items-center">
                 <div className="gap-y-35">
                     <p className="font-semibold text-[60px] md:text-[40]" style={{ fontFamily: 'Plus Jakarta Sans, sans-serif' }}>
-                        Rubah Disini Suara Modern dengan Keamanan Tinggi dan Fleksibilitas
+                        Pemilihan Suara Modern dengan Keamanan Tinggi dan Fleksibilitas
                     </p>
                     <p className="font-normal leading-8 text-[20px] md:text-[10] mt-12" style={{ fontFamily: 'Plus Jakarta Sans, sans-serif' }}>
                         AmanMemilih hadir sebagai solusi dari pengembangan sistem rekapitulasi suara menggunakan teknologi Web 3.0, dengan ini hasil rekapitulasi suara tidak bisa dimanipulasi oleh pihak ketiga.
